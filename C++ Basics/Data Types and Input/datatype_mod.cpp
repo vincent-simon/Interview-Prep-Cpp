23bdf0134a512c13fc5a7545c35c19a6ec4cda65
Vincent Simon <vincentsimon329@gmail.com>
2022-09-10 17:35:01 +0530

    Day 3 Start

---
+++ C++ Basics/Data Types and Input/datatype_mod.cpp
@@ -0,0 +1,65 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int integer = 2147483649;
+//     long int long_integer = 2147483649;
+
+//     cout<<"integer = "<<integer<<endl;
+//     cout<<"long integer = "<<long_integer<<endl;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int integer = 32768;
+//     short int short_integer = 32768;
+
+//     cout<<"integer = "<<integer<<endl;
+//     cout<<"short integer = "<<short_integer<<endl;
+//     return 0;
+// }
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//   // Initialize variables 
+//   int integer = -10;
+//   unsigned int unsigned_integer = -10;
+
+//   char character = 'A';
+//   unsigned char unsigned_character = 'B';
+//   // Display variables value
+//   cout << "integer = " << integer << endl;
+//   cout << "unsigned_integer = " << unsigned_integer << endl;
+
+//   cout << "character = " << character << endl;
+//   cout << "unsigned_character = " << unsigned_character << endl;
+
+// }
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  // Initialize variables
+  int integer = -90;
+  signed int signed_integer = -90;
+
+  char character = 'A';
+  signed char signed_character = 'A';
+  // Display variables value
+  cout << "integer = " << integer << endl;
+  cout << "signed_integer = " << signed_integer << endl;
+
+  cout << "character = " << character << endl;
+  cout << "signed_character = " << signed_character << endl;
+  
+}
---
+++ C++ Basics/Data Types and Input/primitive.cpp
@@ -0,0 +1,30 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int integer_number = 10;
+//     float float_number = 10.5;
+
+//     float float_scientific = 97e4;
+//     double double_number = 10.5;
+
+//     cout<<"int = "<<integer_number<<endl;
+//     cout<<"float = "<<float_number<<endl;
+//     cout<<"float scientific = "<<float_scientific<<endl;
+//     cout<<"double = "<<double_number<<endl;
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char character = 'A';
+    bool boolean  = true;
+
+    cout<<"character = "<<character<<endl;
+    cout<<"boolean = "<<boolean<<endl;
+    return 0;
+}
